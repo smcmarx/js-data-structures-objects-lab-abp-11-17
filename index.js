766cfc5052f76f2b38198a42a83730ae572858aa
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-11 00:04:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  
+ driver[key] = value;
+ return driver
 }
 
 function deleteFromDriverByKey(driver, key) {
-  
+
 }
